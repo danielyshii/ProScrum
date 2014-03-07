9b985a1739fb775e4e62ca41ec07e53ab01e3270
Daniel Yshii <andulink@gmail.com>
2014-03-06 20:36:46 -0500

    Se agregó el menú de TaskBoard y Se corrigio el Accoount Controller.

---
+++ RedCell.ProScrum.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+using RedCell.ProScrum.WebUI.Filters;
+using RedCell.ProScrum.WebUI.Models;
+
+namespace RedCell.ProScrum.WebUI.Controllers
+{
+    [InitializeSimpleMembership]
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
+        {
+            LoginModel review = TempData["LoginModel"] as LoginModel;
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View(review);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult LogOn(LoginModel model, string returnUrl)
+        {
+            TempData["LoginModel"] = model;
+
+            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe))
+            {
+                
+
+                return Redirect(returnUrl);
+            }
+            else if (WebSecurity.UserExists(model.UserName))
+            {
+            
+            }
+            else
+            {
+                WebSecurity.CreateUserAndAccount(model.UserName, model.Password, new { Name = model.UserName });
+                WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe);
+                return Redirect(returnUrl);
+            }
+
+            model.Password = null;
+
+            return RedirectToAction("Login", new { @returnUrl = returnUrl });
+        }
+
+    }
+}
